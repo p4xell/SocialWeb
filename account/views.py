bbf9e4682683ebb459ecb7910980660f3372fdcf
Roman <d4vegame@gmail.com>
2023-03-08 17:01:54 +0300

    account

--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.views import View
 
-# Create your views here.
+
+class UserLogin(View):
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'account/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Authenticated successfully')
+            else:
+                return HttpResponse('Disabled account')
+
+        else:
+            return HttpResponse('Invalid login')
+
+
